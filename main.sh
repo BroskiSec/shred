dddc5a5c6daec4aa98f413cb50c4224844efc113
BroskiSec <broski1800@protonmail.ch>
2022-06-28 15:30:57 -0400

    spelling mistake

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
-echo "shredding file: $1 ; overwritting $2 time(s). ; please wait. . ."
+echo "shredding file: $1 ; overwriting $2 time(s). ; please wait. . ."
 x="$(du -k $1)"; x=($(echo -n $x | tr " " "\t"))
 for elm in "${x[0]}"; do
     dd if=/dev/zero of=$1 count=$elm iflag=count_bytes &>/dev/null
     for ((y = 0; y < $2; y++)); do
         dd if=/dev/random of=$1 count=$(($elm + $RANDOM % 100)) iflag=count_bytes &>/dev/null
     done;
-done; echo "overwitten $2 time(s) ; $1 has been shredded successfully."
+done; echo "overwritten $2 time(s) ; $1 has been shredded successfully."
